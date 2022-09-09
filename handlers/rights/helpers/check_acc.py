92ed8eb42e4ebf656a9dbb1eb69b1019d05bd654
eugene <evgenijgoncarevic@gmail.com>
2022-09-09 11:20:46 +0300

    check access done

---
+++ handlers/rights/helpers/check_acc.py
@@ -0,0 +1,6 @@
+from db_api.get_access_db import get_access_db
+def has_access(user: str)-> bool:
+    """@user - message.from_user.username\n
+        Returns True if user have access to command. False in another case
+    """
+    return user in get_access_db()
