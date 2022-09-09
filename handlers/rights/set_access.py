0b82bed625ad019c99ad20bc034bfc6785b8a63f
eugene <evgenijgoncarevic@gmail.com>
2022-09-09 11:28:13 +0300

    set access fix

--- handlers/rights/set_access.py
+++ handlers/rights/set_access.py
@@ -6,9 +6,10 @@ from db_api.set_access_db import set_access_db
 
 @disp.message_handler(commands=['setacc'])
 async def set_access(message: types.Message):
+    users_from_db = get_access_db()
+    if len(users_from_db) == 0: set_access_db('wlangust') #Change to your
     usernames = message.text.replace(' ','').split('@')[1:]
     if len(usernames) ==0: return await message.answer("Укажите пользователей через @")
-    users_from_db = get_access_db()
     for i in usernames:
         if i in users_from_db: 
             await message.answer(f'Пользователь {i} уже в базе')
