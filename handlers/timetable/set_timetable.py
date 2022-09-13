9cf9a74c14648f9e3705e9d30ad5835e0e971a0e
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-09-14 01:52:26 +0300

    tt fix

--- handlers/timetable/set_timetable.py
+++ handlers/timetable/set_timetable.py
@@ -4,10 +4,13 @@ from db_api.set.set_timetable_db import set_timetable_db
 from handlers.rights.helpers.check_acc import has_access
 
 import re
-@disp.message_handler(commands=['settt'])
+@disp.message_handler(commands=['sett'])
 async def get_timetable(message:types.Message):
     if not has_access(message.from_user.username): return await message.answer("У вас нет прав на эту команду")
-    ref=message.text.split(' ',maxsplit=1)[1]
+    try:
+        ref=message.text.split(' ',maxsplit=1)[1]
+    except Exception:
+        return await message.answer('Неправильный синтаксис команды')
     x=re.match(r'^https://it.belstu.by/wp-content/uploads.*\.pdf$',ref)
     if x is None: return await message.answer('Неправильная ссылка')
     await message.answer('Данные добавлены' if set_timetable_db(ref) else 'Ошибка добавления, отмена команды')
