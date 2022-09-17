f4a30aa77b459be3672d81eba6414124b53e1ced
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-09-17 21:54:42 +0300

    help fix

--- handlers/start/start.py
+++ handlers/start/start.py
@@ -4,4 +4,4 @@ from aiogram import types
 
 @disp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("Привет, я FIT manager. Вот у меня будет куча функций, но я вам их не покажу)\nПишите /help, чтобы узнать")
+    await message.answer("Привет, я FIT manager. У меня тут куча функций\nПишите /help, чтобы узнать о них")
