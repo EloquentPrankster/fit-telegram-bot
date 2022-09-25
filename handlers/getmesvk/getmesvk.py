a65dd00e7ecb09d778a24df6d7aa6603888159b1
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-09-25 14:52:37 +0300

    getmesvk fix

--- handlers/getmesvk/getmesvk.py
+++ handlers/getmesvk/getmesvk.py
@@ -6,5 +6,5 @@ from VK_api.get_messages.get_messages import get_messages
 
 @disp.message_handler(commands=['getmesvk'])
 async def getmesvk(message: types.Message):
-    listMes = get_messages()
+    listMes = await get_messages()
     await transfer_messages(listMes)
