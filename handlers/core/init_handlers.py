9ee203f42e219a227268d9c5dad6f49af0903b11
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-10-04 09:45:35 +0300

    getmesvk removed

--- handlers/core/init_handlers.py
+++ handlers/core/init_handlers.py
@@ -29,5 +29,3 @@ def init_handlers():
     import handlers.timetable.set_timetable
     # help
     import handlers.help.help
-    # getmesvk
-    import handlers.getmesvk.getmesvk
--- handlers/getmesvk/getmesvk.py
+++
@@ -1,8 +0,0 @@
-from aiogram import types
-from bot import disp
-
-from VK_api.get_messages.get_messages import get_messages
-
-@disp.message_handler(commands=['getmesvk'])
-async def getmesvk(message: types.Message):
-    await get_messages()
