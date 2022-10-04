6d68c9a27e52e4dcee6e3bd4a6ef88f44cf0583a
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-10-04 08:22:19 +0300

    taskdef time fix3

--- sheduler/task_definition.py
+++ sheduler/task_definition.py
@@ -5,5 +5,5 @@ from handlers.reminder.helpers.notifier import notifier
 
 def task_definition():
     shed.every().day.at('00:10').do(clean_reminder)
-    shed.every().day.at('08:16').do(notifier)
+    shed.every().day.at('06:00').do(notifier)
     shed.every(15).seconds.do(get_messages)
