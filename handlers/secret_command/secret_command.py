c5bd0629c95a48b89f0a7d01754ef55975822121
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-02-06 13:48:05 +0300

    maxim update

--- handlers/secret_command/secret_command.py
+++ handlers/secret_command/secret_command.py
@@ -1,7 +1,24 @@
+import random
 from bot import disp
 from aiogram import types
 
 
 @disp.message_handler(commands=["maxim"])
 async def shiman_worktime(message: types.Message):
-    await message.answer("Максим Матарас иди нахуй")
+    list_of_max =[
+        "иди нахуй",
+        "гомосек",
+        "отчислен",
+        "петух",
+        "норм чел",
+        "покоритель негроw",
+        "любит негроw",
+        "ебет негроw",
+        "ненавидит негроw",
+        "любит сосать у негроw",
+        "затащил",
+        "построит бассейн"
+    ]
+    range_of_num = list(range(1, len(list_of_max) + 1))
+    random.shuffle(range_of_num)
+    await message.answer("Максим Матарас "+list_of_max[0])
