e761c5b703e2ecba2ff2bd1433af92cbfdcfe46b
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-06-14 17:35:35 +0300

    all hotfix 2

--- handlers/all.py
+++ handlers/all.py
@@ -4,12 +4,12 @@ import services
 
 
 async def all(message: types.Message):
-    usernames = [i for i in services.db_manager.get_bound_usernames()]
+    usernames = [i.username for i in services.db_manager.get_bound_usernames()]
     if (len(usernames) == 0):
         return await message.answer("Список пуст. На фан-встречу никто не придет :[")
     username = message.from_user.username
     if username not in usernames:
         return await message.reply("Используйте /bindme, чтобы получить доступ к команде")
-    res = "".join(f"@{i.username} " for i in usernames)
+    res = "".join(f"@{i} " for i in usernames)
     await message.answer(res)
     logging.info(f"all completed")
