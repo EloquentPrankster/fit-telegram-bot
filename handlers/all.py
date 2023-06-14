5a47080cdccd58710fcb164bdf688a7b5dd71329
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-06-14 17:26:25 +0300

    all command update

--- handlers/all.py
+++ handlers/all.py
@@ -4,7 +4,12 @@ import services
 
 
 async def all(message: types.Message):
-    res = "".join(
-        f"@{i.username} " for i in services.db_manager.get_bound_usernames())
-    await message.answer(res if len(res) != 0 else "Никто не пришел на фан-встречу :[")
+    usernames = services.db_manager.get_bound_usernames()
+    if (len(usernames) == 0):
+        return await message.answer("Список пуст. На фан-встречу никто не придет :[")
+    username = message.from_user.username
+    if username not in usernames:
+        return await message.reply("Используйте /bindme, чтобы получить доступ к команде")
+    res = "".join(f"@{i.username} " for i in usernames)
+    await message.answer(res)
     logging.info(f"all completed")
