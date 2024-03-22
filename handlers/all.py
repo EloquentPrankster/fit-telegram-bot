cc35470d540f83bd7a485fa7157cccaa60843097
EugeneAlWin <evgenijgoncarevic@gmail.com>
2024-03-23 02:45:57 +0300

    all refactor

--- handlers/all.py
+++ handlers/all.py
@@ -1,4 +1,6 @@
+import asyncio
 import logging
+from math import log
 from aiogram import types
 from config import CHAT_ID
 from app import app
@@ -6,11 +8,23 @@ from app import app
 
 async def all(message: types.Message):
     try:
-        administrators = []
-        async for m in app.get_chat_members(message.chat.id):
-            administrators.append(m.user.username)
+        users = []
+        async for m in app.get_chat_members(CHAT_ID):
+            users.append(m.user.username)
 
-        await message.answer("".join(['@'+str(i) + " "for i in administrators]))
+        matrix_with_users = [[] for _ in range(0, (len(users)/5).__ceil__())]
+
+        i = row = 0
+        for user in users:
+            matrix_with_users[row].append(user)
+            i += 1
+            if i % 5 == 0:
+                row += 1
+
+        for i in matrix_with_users:
+            await message.answer("".join(['@'+str(b) + " " for b in i]))
+            await asyncio.sleep(1)
+
         logging.info("All command completed successfully")
     except Exception as e:
         logging.error("All command error")
